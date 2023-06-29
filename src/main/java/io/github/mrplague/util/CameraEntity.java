7c363451491363e964bd0bb7d4383c31b276b4ed
The black split youtube <93332036+TheBlackSplitYt@users.noreply.github.com>
2023-06-29 10:47:02 -0500

    Update CameraEntity.java

--- src/main/java/io/github/mrplague/util/CameraEntity.java
+++ src/main/java/io/github/mrplague/util/CameraEntity.java
@@ -1,9 +1,9 @@
-package io.github.mrplague.util;
+package io.github.split.util;
 
 import com.mojang.authlib.GameProfile;
-import io.github.mrplague.MrPlagueWarper;
-import io.github.mrplague.MrPlagueWarperClient;
-import io.github.mrplague.networking.ModPackets;
+import io.github.split.MrPlagueWarper;
+import io.github.split.MrPlagueWarperClient;
+import io.github.split.networking.ModPackets;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.input.KeyboardInput;
@@ -18,7 +18,7 @@ import net.minecraft.world.dimension.DimensionTypes;
 
 import java.util.UUID;
 
-import static io.github.mrplague.MrPlagueWarperClient.*;
+import static io.github.split.SplitsArcClient.*;
 
 public class CameraEntity extends ClientPlayerEntity {
     double multX;
@@ -60,90 +60,90 @@ public class CameraEntity extends ClientPlayerEntity {
     }
 
     public void renderBlocks() {
-        if (MrPlagueWarperClient.viewed_dim == 2)
+        if (SplitsArcClient.viewed_dim == 2)
         {
-            multX = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? 0 : ((MrPlagueWarperClient.x) / 8 - MrPlagueWarperClient.x));
-            multZ = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? 0 : ((MrPlagueWarperClient.z) / 8 - MrPlagueWarperClient.z));
+            multX = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? 0 : ((SplitsArcClient.x) / 8 - SplitsArcClient.x));
+            multZ = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? 0 : ((SplitsArcClient.z) / 8 - SplitsArcClient.z));
         }
         else
         {
-            multX = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? ((MrPlagueWarperClient.x * 8) - MrPlagueWarperClient.x) : 0);
-            multZ = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? ((MrPlagueWarperClient.z * 8) - MrPlagueWarperClient.z) : 0);
+            multX = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? ((SplitsArcClient.x * 8) - SplitsArcClient.x) : 0);
+            multZ = (client.player.world.getRegistryKey().getValue().equals(DimensionTypes.THE_NETHER_ID) ? ((SplitsArcClient.z * 8) - SplitsArcClient.z) : 0);
         }
         for (int x = -4; x < 5; x++) {
             for (int y = -4; y < 5; y++) {
                 for (int z = -4; z < 5; z++) {
                     PacketByteBuf buf = PacketByteBufs.create();
                     buf.writeBlockPos(new BlockPos(this.getX() + x + multX, this.getY() + y, this.getZ() + z + multZ));
-                    buf.writeInt(MrPlagueWarperClient.viewed_dim);
+                    buf.writeInt(SplitsArcClient.viewed_dim);
                     ClientPlayNetworking.send(ModPackets.SEND_BLOCKPOS, buf);
                 }
             }
         }
         PacketByteBuf bufDim = PacketByteBufs.create();
-        bufDim.writeInt(MrPlagueWarperClient.viewed_dim);
+        bufDim.writeInt(SplitsArcClient.viewed_dim);
         ClientPlayNetworking.send(ModPackets.SEND_PLAYERPOS, bufDim);
     }
 
     public void renderWarpPoints() {
-        if (MrPlagueWarperClient.viewed_dim == 1 && MrPlagueWarperClient.enabled && MrPlagueWarperClient.cameraEntity != null) {
+        if (SplitsArcClient.viewed_dim == 1 && SplitsArcClient.enabled && SplitsArcClient.cameraEntity != null) {
             if (warp1_dim == 1) {
-                MC.particleManager.addParticle(MrPlagueWarper.OVERWORLD_WARP, warp1_x - (int)multX + 0.5, warp1_y + 0.5, warp1_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.OVERWORLD_WARP, warp1_x - (int)multX + 0.5, warp1_y + 0.5, warp1_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp2_dim == 1) {
-                MC.particleManager.addParticle(MrPlagueWarper.OVERWORLD_WARP, warp2_x - (int)multX + 0.5, warp2_y + 0.5, warp2_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.OVERWORLD_WARP, warp2_x - (int)multX + 0.5, warp2_y + 0.5, warp2_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp3_dim == 1) {
-                MC.particleManager.addParticle(MrPlagueWarper.OVERWORLD_WARP, warp3_x - (int)multX + 0.5, warp3_y + 0.5, warp3_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.OVERWORLD_WARP, warp3_x - (int)multX + 0.5, warp3_y + 0.5, warp3_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp4_dim == 1) {
-                MC.particleManager.addParticle(MrPlagueWarper.OVERWORLD_WARP, warp4_x - (int)multX + 0.5, warp4_y + 0.5, warp4_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.OVERWORLD_WARP, warp4_x - (int)multX + 0.5, warp4_y + 0.5, warp4_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp5_dim == 1) {
-                MC.particleManager.addParticle(MrPlagueWarper.OVERWORLD_WARP, warp5_x - (int)multX + 0.5, warp5_y + 0.5, warp5_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.OVERWORLD_WARP, warp5_x - (int)multX + 0.5, warp5_y + 0.5, warp5_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp6_dim == 1) {
-                MC.particleManager.addParticle(MrPlagueWarper.OVERWORLD_WARP, warp6_x - (int)multX + 0.5, warp6_y + 0.5, warp6_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.OVERWORLD_WARP, warp6_x - (int)multX + 0.5, warp6_y + 0.5, warp6_z - (int)multZ + 0.5, 0, 0, 0);
             }
         }
-        if (MrPlagueWarperClient.viewed_dim == 2 && MrPlagueWarperClient.enabled && MrPlagueWarperClient.cameraEntity != null) {
+        if (SplitsArcClient.viewed_dim == 2 && SplitsArcClient.enabled && SplitsArcClient.cameraEntity != null) {
             if (warp1_dim == 2) {
-                MC.particleManager.addParticle(MrPlagueWarper.NETHER_WARP, warp1_x - (int)multX + 0.5, warp1_y + 0.5, warp1_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.NETHER_WARP, warp1_x - (int)multX + 0.5, warp1_y + 0.5, warp1_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp2_dim == 2) {
-                MC.particleManager.addParticle(MrPlagueWarper.NETHER_WARP, warp2_x - (int)multX + 0.5, warp2_y + 0.5, warp2_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.NETHER_WARP, warp2_x - (int)multX + 0.5, warp2_y + 0.5, warp2_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp3_dim == 2) {
-                MC.particleManager.addParticle(MrPlagueWarper.NETHER_WARP, warp3_x - (int)multX + 0.5, warp3_y + 0.5, warp3_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.NETHER_WARP, warp3_x - (int)multX + 0.5, warp3_y + 0.5, warp3_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp4_dim == 2) {
                 MC.particleManager.addParticle(MrPlagueWarper.NETHER_WARP, warp4_x - (int)multX + 0.5, warp4_y + 0.5, warp4_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp5_dim == 2) {
-                MC.particleManager.addParticle(MrPlagueWarper.NETHER_WARP, warp5_x - (int)multX + 0.5, warp5_y + 0.5, warp5_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.NETHER_WARP, warp5_x - (int)multX + 0.5, warp5_y + 0.5, warp5_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp6_dim == 2) {
-                MC.particleManager.addParticle(MrPlagueWarper.NETHER_WARP, warp6_x - (int)multX + 0.5, warp6_y + 0.5, warp6_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.NETHER_WARP, warp6_x - (int)multX + 0.5, warp6_y + 0.5, warp6_z - (int)multZ + 0.5, 0, 0, 0);
             }
         }
-        if (MrPlagueWarperClient.viewed_dim == 3 && MrPlagueWarperClient.enabled && MrPlagueWarperClient.cameraEntity != null) {
+        if (SplitsArcClient.viewed_dim == 3 && SplitsArcClient.enabled && SplitsArcClient.cameraEntity != null) {
             if (warp1_dim == 3) {
-                MC.particleManager.addParticle(MrPlagueWarper.END_WARP, warp1_x - (int)multX + 0.5, warp1_y + 0.5, warp1_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.END_WARP, warp1_x - (int)multX + 0.5, warp1_y + 0.5, warp1_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp2_dim == 3) {
-                MC.particleManager.addParticle(MrPlagueWarper.END_WARP, warp2_x - (int)multX + 0.5, warp2_y + 0.5, warp2_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.END_WARP, warp2_x - (int)multX + 0.5, warp2_y + 0.5, warp2_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp3_dim == 3) {
-                MC.particleManager.addParticle(MrPlagueWarper.END_WARP, warp3_x - (int)multX + 0.5, warp3_y + 0.5, warp3_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.END_WARP, warp3_x - (int)multX + 0.5, warp3_y + 0.5, warp3_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp4_dim == 3) {
-                MC.particleManager.addParticle(MrPlagueWarper.END_WARP, warp4_x - (int)multX + 0.5, warp4_y + 0.5, warp4_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.END_WARP, warp4_x - (int)multX + 0.5, warp4_y + 0.5, warp4_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp5_dim == 3) {
-                MC.particleManager.addParticle(MrPlagueWarper.END_WARP, warp5_x - (int)multX + 0.5, warp5_y + 0.5, warp5_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.END_WARP, warp5_x - (int)multX + 0.5, warp5_y + 0.5, warp5_z - (int)multZ + 0.5, 0, 0, 0);
             }
             if (warp6_dim == 3) {
-                MC.particleManager.addParticle(MrPlagueWarper.END_WARP, warp6_x - (int)multX + 0.5, warp6_y + 0.5, warp6_z - (int)multZ + 0.5, 0, 0, 0);
+                MC.particleManager.addParticle(splitsarc.END_WARP, warp6_x - (int)multX + 0.5, warp6_y + 0.5, warp6_z - (int)multZ + 0.5, 0, 0, 0);
             }
         }
     }
